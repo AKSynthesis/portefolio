88b04fd5c9fbb8da169bf04c9600ffec10ba5679
AKSynthesis <kevinbieleu@gmail.com>
2025-08-30 19:23:07 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold text-white underline">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
