6a975e6d2fa1c41c81a5222ff27c47e0ff583708
AKSynthesis <kevinbieleu@gmail.com>
2025-08-31 12:57:27 -0300

    v1.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,26 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Navbar from "./components/Navbar.jsx"
+import Hero from "./components/Hero.jsx"
+import Technologies from "./components/Technologies.jsx"
+import Experiences from "./components/Experiences.jsx"
+import Projects from "./components/Projects.jsx"
+import Contact from "./components/Contact.jsx"
 
 function App() {
 
   return (
-    <>
-      <h1 className="text-3xl font-bold text-white underline">
-        Hello world!
-      </h1>
-    </>
+    <div className="overflow-x-hidden text-stone-300 antialiased scroll-smooth">
+      <div className="fixed inset-0 -z-10 h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_100%_100%_at_50%_-10%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+
+      {/* Page content */}
+      <div className="container mx-auto px-8">
+        <Navbar />
+        <Hero />
+        <Technologies />
+        <Projects />
+        <Experiences />
+        <Contact />
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/Hero.jsx
@@ -0,0 +1,92 @@
+import { motion } from "framer-motion";
+import profilePic from "../assets/raviKumarProfile.webp"
+import { HERO_CONTENT } from "../constants"
+
+const containerVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: {
+            duration: 0.5,
+            staggerChildren: 0.5,
+        }
+    }
+}
+
+const ChildVariants = {
+    hidden: { opacity: 0, x: -100 },
+    visible: {
+        opacity: 1,
+        x: 0,
+        transition: { duration: 0.5, }
+    }
+}
+
+function Hero() {
+    return (
+        <div className="pb-4 lg:mb-36">
+            {/* Flex container for image + content */}
+            <div className="flex flex-wrap lg:flex-row-reverse items-center">
+
+                {/* Profile Image */}
+                <div className="w-full lg:w-1/2">
+                    <div className="flex justify-center lg:p-8">
+                        <motion.img
+                            src={profilePic}
+                            alt="Armel Kevin"
+                            className="border border-stone-900 rounded-3xl"
+                            width={650}
+                            height={650}
+                            initial={{ x: 100, opacity: 0 }}
+                            animate={{ x: 0, opacity: 1 }}
+                            transition={{ duration: 1, delay: 1.5 }}
+                        />
+                    </div>
+                </div>
+
+                {/* Text Content */}
+                <div className="w-full lg:w-1/2">
+                    <motion.div
+                        initial="hidden"
+                        animate="visible"
+                        variants={containerVariants}
+                        className="flex flex-col items-center lg:items-start mt-10 lg:mt-0"
+                    >
+                        <motion.h2
+                            variants={ChildVariants}
+                            className="pb-2 text-4xl tracking-tighter lg:text-8xl"
+                        >
+                            Armel Kevin
+                        </motion.h2>
+                        <motion.span
+                            variants={ChildVariants}
+                            className="bg-gradient-to-r from-stone-300 to-stone-600 bg-clip-text text-3xl tracking-tight text-transparent"
+                        >
+                            Full Stack Developer
+                        </motion.span>
+                        <motion.p
+                            variants={ChildVariants}
+                            className="my-2 max-w-lg py-6 text-xl leading-relaxed tracking-tighter"
+                        >
+                            {HERO_CONTENT}
+                        </motion.p>
+                        <motion.a
+                            variants={ChildVariants}
+                            href="/resume.pdf"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            download
+                            className="bg-white rounded-full px-6 py-3 text-sm font-medium text-stone-800 shadow"
+                        >
+                            Download Resume
+                        </motion.a>
+                    </motion.div>
+                </div>
+
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
